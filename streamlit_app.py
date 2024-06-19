c48aab286f18fe4e0955535eeb40e102891fd585
jingane <jingane.2024k@gmail.com>
2024-06-19 23:30:54 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,10 +91,10 @@ else:
     with col1:
         st.subheader("오늘 외부")
         external_today = st.text_area(" ", schedule.external_today or "", key="external_today", height=300)
-        st.subheader("내일 내부")
+        st.subheader("오늘 외부")
         internal_tomorrow = st.text_area(" ", schedule.internal_tomorrow or "", key="internal_tomorrow", height=300)
     with col2:
-        st.subheader("오늘 내부")
+        st.subheader("내일 내부")
         internal_today = st.text_area(" ", schedule.internal_today or "", key="internal_today", height=300)
         st.subheader("내일 외부")
         external_tomorrow = st.text_area(" ", schedule.external_tomorrow or "", key="external_tomorrow", height=300)
